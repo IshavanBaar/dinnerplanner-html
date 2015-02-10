2b5cfecb83de125897d871ccfc88187de9e9d184
Isha van Baar <isha.vanbaar@gmail.com>
2015-02-10 12:29:43 +0100

    Filled dish description page with real data, added some buttons

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -1,7 +1,12 @@
 // View for home page
 var HomeView = function (container, model) {	
-
-	// Temporary - used to show selected page
+	
+	// Variable to manage current dish.
+	this.currentDish; 
+	this.currentDishQuantity; // TODO - something with this.
+	this.currentDishFilled = false; // TODO does not work, find another way to do this
+	
+	// TODO hardcoded, not get away? - used to show selected page
 	$('.page-switch-button').click(function() {
 		// hide all other pages
 		$('.page').hide();
@@ -21,12 +26,12 @@ var HomeView = function (container, model) {
 	}
 	
 	// Confirm dish by adding it to list
-	$('.confirm-dish-button').click(function addDishToList() {
-		var tableRef = $('#dinnerCheckList tbody');
-		// Hardcoded values for now 
-		var quantity = 4;
-		var dishName = 'Meatballs';
-		var dishCosts = 77.70;
+	$('#confirm-dish-button').click(function addDishToList() {
+		var tableRef = $('#dinnerCheckList tbody'); 
+		
+		var quantity = -999; // TODO - hardcoded values for now
+		var dishName = currentDish.name;
+		var dishCosts = getCurrentDishPrice(currentDish);
 		
 		// Add row to table
 		tableRef.prepend('<tr class="align-left"><td>'+quantity+'</td><td class="align-center">'+dishName+'</td><td class="align-right">'+dishCosts+'</td></tr>');
@@ -37,6 +42,7 @@ var HomeView = function (container, model) {
 		$('#totalCosts').text(newCosts);
 	});
 	
+	// Fills available dishes in dish selection screen
 	$('#availableDishes').ready(function fillAvailableDishes() {
 		var fullMenu = model.getAllDishes();
 		var numberOfMenuItems = fullMenu.length;
@@ -47,22 +53,101 @@ var HomeView = function (container, model) {
 			var dishImage = 'images/'+ dish.image;
 			var dishDescription = dish.description.substr(0, 37) + '...';
 			
-			$('#availableDishes').append('<li>'
-											+ '<a href="#" dish_id="'+dishId+'" class="dish-item"><img class="dish-image" src="'+dishImage+'" alt="'+dishName+'"/>'
-											+ '<div class="dish-name">'+dishName+'</div></a>'
-											+ '<div class="dish-description">'+dishDescription+'</div>'
-										+ '</li>');
+			$('#availableDishes').append(
+				'<li>'+
+					'<a href="#" dish-id="'+dishId+'" class="dish-item"><img class="dish-image" src="'+dishImage+'" alt="'+dishName+'"/>'+
+					'<div class="dish-name">'+dishName+'</div></a>'+
+					'<div class="dish-description">'+dishDescription+'</div>'+
+				'</li>'
+			);
 		}
-
-
-		// Temporary - hard code - show dish detail page when clicking a dish
+		
+		// Show dish detail page when clicking a dish
 		$('a.dish-item').click(function() {
-			// hide all other pages
+			// Get dish clicked
+			var dishId = $(this).attr('dish-id');
+			currentDish = model.getDish(dishId);
+			
+			// Needed fields for dish detail page
+			var dishName = currentDish.name;
+			var dishImage = 'images/'+ currentDish.image;
+			var dishDescription = currentDish.description.substr(0, 37) + '...'; 
+			var dishIngredients = currentDish.ingredients;
+			var dishPreparation = currentDish.description.substr(0, 37) + '...'; // No preparation field in dishes array?!
+			
+			
+			// Fill dish detail page if not filled already
+			//if (this.currentDishFilled === false) {
+				//TODO find another way to do this, this does not work
+				fillPageDishDetail(dishName, dishImage, dishDescription, dishIngredients, dishPreparation);
+			//}
+			
+			// Hide all other pages
 			$('.page').hide();
 			
-			// show selected page
+			// Show selected page
 			$('#pageDishDetail').show();
 		});
+		
+		// Fills dish detail page
+		function fillPageDishDetail(dishName, dishImage, dishDescription, dishIngredients, dishPreparation) {
+			// Fill dish name and description fields
+			$('#dishNameDescription').append(
+				'<h2 class="page-header">'+dishName+'</h2>'+
+				'<p><img class="dish-image" src="'+dishImage+'"/></p>'+
+				'<p class="dish-description">'+dishDescription+'</p>'+
+				'<p class="align-left"><button location="toPageSelectDish" class="page-switch-button">back to Select Dish</button></p>'
+			);
+
+			// Fill dish ingredients table
+			var numberOfIngredients = dishIngredients.length;
+			for (var i = 0; i<numberOfIngredients; i++) {
+				var ingredient = dishIngredients[i];
+				
+				var ingredientName = ingredient.name;
+				var ingredientQuantity = ingredient.quantity;
+				var ingredientUnit = ingredient.unit;
+				var ingredientPrice = ingredient.price;
+			
+				$('#dishIngredients').append(
+					'<tr>'+
+						'<td>'+ingredientQuantity+'</td>'+
+						'<td>'+ingredientName+'</td>'+
+						'<td>'+ingredientUnit+'</td>'+
+						'<td class="align-right">'+ingredientPrice+'</td>'+
+					'</tr>'
+				);	
+			}
+
+			// Fill preparation field 	
+			$('#dishPreparation').append(			
+				'<h2 class="page-header">Preparation</h2>'+			
+				'<p class="dish-preparation">'+dishPreparation+'</p>'	
+			)
+			
+			// Put on true, because the dish page has been filled with data
+			//this.currentDishFilled = true; TODO find another way to do this
+		}
 	});
+		
+	
+	
+	function getCurrentDishPrice(dish) {
+		var dishIngredients = dish.ingredients;
+		var currentDishPrice = 0.0;
+		var numberOfIngredients = dishIngredients.length;
+		for (var i = 0; i<numberOfIngredients; i++) {
+			var ingredient = dishIngredients[i];
+			
+			var ingredientName = ingredient.name;
+			var ingredientQuantity = ingredient.quantity;
+			var ingredientUnit = ingredient.unit;
+			var ingredientPrice = ingredient.price;
+			
+			// Manage dishPrice
+			currentDishPrice += ingredientPrice;
+		}
+		return Math.round(currentDishPrice * 10)/10;
+	}
 }
  
